b431ccc1867275f0fb26931e08936e6c5365cc0b
AniketGadge2024 <aniketgadge2024@gmail.com>
2024-08-14 13:26:57 +0530

    mani

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import {  } from 'react'
-
+import React from 'react'
 import './App.css'
-import Hr from './Component/HR/Hr'
-import DataHome from './Component/DataScience/DataHome'
-import Marketing from './Component/Marketing/Marketing'
-import Ui from './Component/Design/Ui'
-import Finance from './Component/Finance/Finance'
-import Fron from './Component/Front-end/Fron'
-import Ai from './Component/AIML/Ai'
-import Full from './Component/Fullstack/Full'
-
-
-function App() {
-  
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+const App = () => {
   return (
-    <>
-      <div >
-        {/* <Hr/> */}
-        {/* <DataHome/> */}
-        {/* <Marketing/> */}
-        {/* <Ui/> */}
-        {/* <Finance/> */}
-        {/* <Fron/> */}
-        {/* <Ai/> */}
-        <Full/>
-      </div>
-      
-    </>
+    <div className='main-div'>
+<div className='inner-div'>
+              
+            <h3 class="text-[#0d151c] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Refund Policy</h3>
+            <p class="text-[#0d151c] text-base font-normal leading-normal pb-3 pt-1 px-4">
+              You may cancel your registration up to 7 days before the event start date for a full refund. We do not provide refunds for cancellations made within 7 days of the
+              event start date.Refunds will be processed within 7-10 business days after the cancellation date. For any refund-related queries, please contact our support team.
+            </p>
+
+
+            <h3 class="text-[#0d151c] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Attendance Policy</h3>
+            <p class="text-[#0d151c] text-base font-normal leading-normal pb-3 pt-1 px-4">
+              You are required to attend at least 80% of the workshop sessions to be eligible for a certificate. Any missed sessions must be notified in advance to the workshop
+              coordinator.
+            </p>
+
+            </div>
+    </div>
   )
 }
 
